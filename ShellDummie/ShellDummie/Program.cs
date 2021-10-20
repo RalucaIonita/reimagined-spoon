731ebbc004a1dd66d93f0bdc0debd3518970cabd
RalucaIonita <raluca.ionita04@gmail.com>
2021-10-20 16:14:12 +0300

    [Initial]

---
+++ ShellDummie/ShellDummie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShellDummie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
